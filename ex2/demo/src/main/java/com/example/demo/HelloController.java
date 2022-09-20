6253c379b3a2831828429573c5b29d21fefc6535
Nadezhda Ryabova <113939581+NadyanR@users.noreply.github.com>
2022-09-20 22:07:55 +0300

    Add files via upload

---
+++ ex2/demo/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+    @GetMapping("/hello")
+    public String index1(){
+        return "It's my first spring boot application";
+    }
+}
